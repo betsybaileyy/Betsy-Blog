7f3cb72ae442835170ab4a9b15839463cd50ccad
betsybaileyy <betsy.bailey80@gmail.com>
2018-12-04 11:50:04 -0800

    users can see all posts

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+// movie = subject, review = post
+
+
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
+
+let posts = [
+    { title: "Great Post", subjectTitle: "Dogs" },
+    { title: "Awesome Subject", subjectTitle: "Boats"}
+]
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' })
+    res.render('posts-index', { posts: posts });
 });
 
 app.listen(3000, () => {
